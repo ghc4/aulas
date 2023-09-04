345af82b4329d88c7afc58203782d1b6ec0922ca
GustavoHC <gustavohc@gmail.com>
2023-09-04 00:23:49 -0300

    Update Classes01.cpp

--- CPP/Basico/Classes01.cpp
+++ CPP/Basico/Classes01.cpp
@@ -1,29 +1,63 @@
 #include <iostream>
+#include <string>
 using namespace std;
-// Classe
 class Caixa {
     // Membros públicos
     public:
-        double comprimento = 0.0;
-        double profundidade = 0.0;
-        double altura = 0.0;
+    	string nome;
+        double comprimento;
+        double profundidade;
+        double altura;
         // Declaração do método dentro da definição da classe
         double volume() {
             return comprimento * profundidade * altura;
         }
         // Declaração para implementação fora da classe
         double base();
+        Caixa(string n, double c, double p, double a);
+        Caixa();
+        void imprimeDados();
 };
-// Implementação de método
+
+// Implementação do construtor 
+Caixa::Caixa(string n, double c, double p, double a) {
+    comprimento = c;
+    profundidade = p;
+    altura = a;
+    nome = n;
+}
+
+// Implementação do construtor 
+Caixa::Caixa() {
+    comprimento = 0;
+    profundidade = 0;
+    altura = 0;
+    nome = "Sem nome";
+}
+
+// Implementação do método
 double Caixa::base() {
     return comprimento * profundidade;
 }
+
+// Implementação do método
+void Caixa::imprimeDados() {
+	printf("--------------------------------------------\n");
+    printf("Nome...........: %s\n",    nome.c_str());
+	printf("Comprimento....: %10.2lf\n", comprimento);
+    printf("Profundidade...: %10.2lf\n", profundidade);
+    printf("Altura.........: %10.2lf\n", altura);
+    printf("Volume.........: %10.2lf\n", volume());
+    printf("Base...........: %10.2lf\n", base());
+    printf("--------------------------------------------\n");
+}
+
+// Código
 int main() {
-   Caixa c;          
-   c.altura = 5.0; 
-   c.comprimento = 6.0; 
-   c.profundidade = 7.0;
-   cout << c.volume() <<endl;
-   cout << c.base() <<endl;
+   Caixa caixaDeSapato("Caixa de sapato", 5.1, 4.2, 3.3123);  
+   caixaDeSapato.imprimeDados();
+   Caixa caixaVazia;
+   caixaVazia.imprimeDados();
    return 0;
 }
+
