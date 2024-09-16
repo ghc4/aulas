3908fae98acb79c9b14abfedc1438792dc7f38fa
GustavoHC <gustavohc@gmail.com>
2024-09-16 20:52:03 -0300

    xx

--- C/EstruturaDeDados/InverteStringRes.c
+++ C/EstruturaDeDados/InverteStringRes.c
@@ -5,8 +5,7 @@ int main() {
     printf("Digite uma string (pressione Enter para finalizar): ");
     while (tamanho<19 && caractere!='\n') normal[tamanho++] = caractere = getchar();
     normal[tamanho--] = '\0';
-    while(tamanho>0) 
-        invertida[contador++] = normal[--tamanho];
+    while(tamanho>0) invertida[contador++] = normal[--tamanho];
     invertida[contador] = '\0';
     printf("String invertida: %s\n", invertida);
     return 0;
